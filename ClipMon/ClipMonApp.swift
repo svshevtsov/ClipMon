17d996a32e9256185eeab4a004727ca0b3a4478a
Sergey Shevtsov <svshevtsov@yandex-team.ru>
2025-08-20 18:16:55 +0200

    Initial Commit

---
+++ ClipMon/ClipMonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipMonApp.swift
+//  ClipMon
+//
+//  Created by Sergey Shevtsov on 20.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ClipMonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipMonTests/ClipMonTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipMonTests.swift
+//  ClipMonTests
+//
+//  Created by Sergey Shevtsov on 20.08.2025.
+//
+
+import Testing
+@testable import ClipMon
+
+struct ClipMonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
